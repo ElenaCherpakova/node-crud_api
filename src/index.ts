1c2843ce416b396ad5597bf53e14c6a39fcbe232
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-10-21 10:35:02 -0400

    add api tests

--- src/index.ts
+++ src/index.ts
@@ -8,23 +8,29 @@ if (!process.env.PORT) {
   process.exit(1);
 }
 
-const server = createServer(
-  async (req: IncomingMessage, res: ServerResponse) => {
-    try {
-      await userRoutes(req, res);
-    } catch (error) {
-      console.error(error);
-      res.writeHead(HttpStatusCode.INTERNAL_SERVER, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Internal server error' }));
-    }
-  },
-);
-const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+export const handleRequest = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  try {
+    await userRoutes(req, res);
+  } catch (error) {
+    console.error(error);
+    res.writeHead(HttpStatusCode.INTERNAL_SERVER, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify({ message: 'Internal server error' }));
+  }
+};
+if (process.env.NODE_ENV !== 'test') {
+  const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
+  const server = createServer(handleRequest);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 
-server.on('error', (err) => {
-  console.log('Server Error', err);
-});
+  server.on('error', (err) => {
+    console.log('Server Error', err);
+  });
+}
